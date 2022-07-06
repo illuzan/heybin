83d0326ac86ba4f18ee49a8760c57c7d334acc5b
Gaurav Singh <allstargaurav@gmail.com>
2022-07-07 00:28:16 +0530

    feat: update homepage

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,11 +1,46 @@
-export default function Index() {
+import { ActionFunction, redirect } from '@remix-run/cloudflare'
+import { Form, useActionData, useTransition } from '@remix-run/react'
+import { nanoid } from 'nanoid/non-secure'
+import { useEffect } from 'react'
+
+export const action: ActionFunction = async ({ context, request }) => {
+  const formData = await request.formData()
+  const textArea = formData.get('text-area')
+  const key = nanoid(8)
+  const urlRegex =
+    /^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/im
+
+  if (typeof textArea === 'string') {
+    if (urlRegex.test(textArea)) {
+      const response = { type: 'url', data: textArea }
+      const responseJSON = JSON.stringify(response)
+      await context.HEYBIN_KV.put(key, responseJSON)
+      const url = `${new URL(request.url).origin}/${key}`
+      return { type: 'url', data: url, slug: key }
+    } else {
+      const response = { type: 'code', data: textArea }
+      const responseJSON = JSON.stringify(response)
+      await context.HEYBIN_KV.put(key, responseJSON)
+      return redirect(`/${key}`)
+    }
+  }
+}
+
+export const Header = ({
+  showSaveButton = false,
+}: {
+  showSaveButton?: boolean
+}) => {
   return (
-    <>
-      <header className='flex items-center justify-between px-4 py-3 mt-4 text-xl border-b shadow-md bg-neutral-50 rounded-xl'>
-        <a href='/' className='font-semibold text-[#393939] '>
-          <span className='text-[#008FF8]'>&lt;Hey</span>bin/&gt;
-        </a>
-        <button className='text-base'>
+    <header className='flex items-center justify-between px-4 py-3 mt-4 text-xl border-b shadow-md bg-neutral-50 rounded-xl'>
+      <a
+        href='/'
+        className='font-semibold text-[#393939] hover:underline  decoration-[#008FF8]'
+      >
+        <span className='text-[#008FF8]'>&lt;Hey</span>bin/&gt;
+      </a>
+      {showSaveButton && (
+        <button form='textareaForm' className='text-base' type='submit'>
           {' '}
           <svg
             className='w-6 h-6'
@@ -40,20 +75,96 @@ export default function Index() {
             ></path>
           </svg>
         </button>
-      </header>
-      <main className='h-full bg-white rounded-xl'>
-        <textarea
-          className='w-full h-full px-6 py-4 font-bold bg-white outline-none resize-none rounded-xl'
-          placeholder='> Paste, save, share! (Pasting just a URL will shorten it!)'
-        ></textarea>
+      )}
+    </header>
+  )
+}
+
+export const Footer = () => {
+  return (
+    <footer className=' flex justify-between px-4 py-3 !mb-4 text-base font-bold border-b shadow-md bg-neutral-50   rounded-xl'>
+      <a className='hover:underline' href='https://gaurav.wtf'>
+        {' '}
+        © {new Date().getFullYear()} Gaurav Singh
+      </a>
+      <a
+        className='text-[#008FF8] hover:underline'
+        href='https://github.com/illuzan/heybin'
+      >
+        Fork me!
+      </a>
+    </footer>
+  )
+}
+
+export default function Index() {
+  const actionData = useActionData()
+  const transition = useTransition()
+
+  const state: 'idle' | 'success' | 'error' | 'submitting' =
+    transition.submission
+      ? 'submitting'
+      : actionData?.type === 'url'
+      ? 'success'
+      : actionData?.error
+      ? 'error'
+      : 'idle'
+
+  useEffect(() => {
+    const handleClick = (event) => {
+      if (
+        (window.navigator.platform.match('Mac')
+          ? event.metaKey
+          : event.ctrlKey) &&
+        event.keyCode == 83
+      ) {
+        event.preventDefault()
+        document.getElementById('textareaForm').submit()
+      }
+    }
+    document.addEventListener('keydown', handleClick)
+
+    return () => {
+      document.removeEventListener('keydown', handleClick)
+    }
+  }, [])
+
+  return (
+    <>
+      <Header showSaveButton={true} />
+      <main className='h-full bg-white border-b shadow-md rounded-xl'>
+        <Form
+          replace
+          method='post'
+          id='textareaForm'
+          className='h-full'
+          hidden={state === 'success'}
+        >
+          <textarea
+            className='w-full h-full px-6 py-4 font-bold tracking-wide bg-white outline-none resize-none rounded-xl disabled:opacity-75'
+            placeholder='> Paste, save, share! (Pasting just a URL will shorten it!)'
+            required={true}
+            name='text-area'
+            tabIndex={state === 'success' ? -1 : 0}
+          ></textarea>
+        </Form>
+        <div
+          hidden={state !== 'success'}
+          className='w-full h-full px-6 py-4 font-bold tracking-wide bg-white outline-none resize-none rounded-xl disabled:opacity-75'
+        >
+          {}
+          <span>
+            Your shortened url is:{' '}
+            <a
+              className='text-[#008FF8] hover:underline'
+              href={actionData?.slug}
+            >
+              {actionData?.data}
+            </a>
+          </span>
+        </div>
       </main>
-      <footer className='flex justify-between px-4 py-3 !mb-4 text-xs font-bold bg-neutral-50 sm:text-base text-amber rounded-xl'>
-        <a href='https://gaurav-singh.com'>
-          {' '}
-          © {new Date().getFullYear()} Gaurav Singh
-        </a>
-        <a href='https://github.com/illuzan/heybin'>Fork me!</a>
-      </footer>
+      <Footer />
     </>
   )
 }
